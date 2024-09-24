e981873d65bfde5ab513b7130d983f28eedf6f0e
Nicolas Sacandy <nicsacandy@gmail.com>
2024-09-24 11:48:54 -0400

    Fixed main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io;
+
+fn main() -> io::Result<()> {
+    // Specify the file path
+    let file_path = "../data/data/CTONGA/branch_weekly_sales.txt";
+    
+    // Read the file content into a String
+    let contents = fs::read_to_string(file_path)?;
+
+    // Print the file content
+    println!("File Contents:\n{}", contents);
+    
+    Ok(())
 }
