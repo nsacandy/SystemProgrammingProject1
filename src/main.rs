532b4d2938d43f66423dab831293c5e59a0def3e
Md Mostafizur Rahman <mr00096@my.westga.edu>
2024-09-26 20:35:07 -0400

    Added new folder for sale text

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 
 fn main() -> io::Result<()> {
     // Specify the file path
-    let file_path = "../data/data/CTONGA/branch_weekly_sales.txt";
+    let file_path = "..\\data/data/CTONGA/branch_weekly_sales.txt";
     
     // Read the file content into a String
     let contents = fs::read_to_string(file_path)?;
