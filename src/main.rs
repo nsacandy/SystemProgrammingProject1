7d6ec09c1f6d8f7a612c5b4c4af5a12cb86b0ea2
Nicolas Sacandy <nicsacandy@gmail.com>
2024-10-01 11:55:55 -0400

    Trying to make Rust happy

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 use std::fs;
 use std::io;
+use csv::ReaderBuilder;
+use std::fs::File;
 
 fn main() -> io::Result<()> {
     // Specify the file path
-    let file_path = "..\\data/data/CTONGA/branch_weekly_sales.txt";
-    
+    let file_path = "..data/data/CTONGA/branch_weekly_sales.txt";
+    let file = File::open(file_path)?;
     // Read the file content into a String
-    let contents = fs::read_to_string(file_path)?;
 
+
+    // Create the CSV reader (without headers) from the file
+    let mut rdr = ReaderBuilder::new().has_headers(false).from_reader(file);
+
+    // Iterate over each record
+    for result in rdr.records() {
+        let record = result?;
+
+        // Print the fields of each record
+        println!("{} {} {} {}", &record[0], &record[1], &record[2], &record[3]);
+    }
     // Print the file content
-    println!("File Contents:\n{}", contents);
+    //println!("File Contents:\n{}", contents);
     
     Ok(())
 }
