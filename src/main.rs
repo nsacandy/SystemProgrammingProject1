0498680c21d46af2284178cd0838c1aced0a9cae
demmons1 <123836293+demmons1@users.noreply.github.com>
2024-10-01 14:55:44 -0400

    added code to parse int from String. First creates seperate string from &record[2] and then trims it before creating an int from parsing.

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,20 @@ fn main() -> io::Result<()> {
     // Create the CSV reader (without headers) from the file
     let mut rdr = ReaderBuilder::new().has_headers(false).from_reader(file);
 
+   
     // Iterate over each record
     for result in rdr.records() {
         let record = result?;
+        let sales_string = &record[2].trim();
+        
+    // Converting a String to an i32
+        let total_sales = sales_string.parse::<i32>().unwrap();
 
         // Print the fields of each record
         println!("{} {} {} {}", &record[0], &record[1], &record[2], &record[3]);
+        println!("{}", total_sales);
     }
+
     // Print the file content
     //println!("File Contents:\n{}", contents);
     
