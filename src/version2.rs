240ff8f266bf231ec84e074ea216d4c50c7e2eb2
Md Mostafiur Rahman <mr00096@my.westga.edu>
2024-10-13 12:09:13 -0400

    Added functionality for the version two in the main.rs file.

--- src/version2.rs
+++ src/version2.rs
@@ -1,3 +1,35 @@
+// mod lib;
+// use lib::process_input_file;
+// use std::fs::OpenOptions;
+// use std::io::{self, Write};
+// use std::path::Path;
+// use std::time::Instant;
+
+// fn main() -> io::Result<()> {
+//     // Start the timer
+//     let base_dir = "../data";
+
+//     // Define the output file path
+//     let output_file_path = "../data/weekly_summary/weekly_sales_summary.txt";
+
+//     let output_file = Path::new(output_file_path);
+//     if output_file.exists() {
+//         // Open the file with the truncate option to clear its contents
+//         OpenOptions::new()
+//             .write(true)
+//             .truncate(true)
+//             .open(output_file_path)?;
+//     }
+
+//     let start_time = Instant::now();
+
+//     // Call the process_input_file function
+//     process_input_file(base_dir, output_file_path)?;
+
+//     let duration = start_time.elapsed();
+//     println!("Time elapsed: {}\nPhew! I am done.", duration.as_secs_f64());
+//     Ok(())
+// }
 mod lib;
 use lib::process_input_file;
 use std::fs::OpenOptions;
