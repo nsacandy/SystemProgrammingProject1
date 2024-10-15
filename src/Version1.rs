20b370a84334605493f3dfdcdab101d78cd45cd3
Nicolas Sacandy <nicsacandy@gmail.com>
2024-10-15 12:47:20 -0400

    Version 1 now running correctly

--- src/Version1.rs
+++ src/Version1.rs
@@ -1,9 +1,10 @@
  mod lib;
  use lib::process_input_file;
- use std::fs::OpenOptions;
+
  use std::io::{self, Write};
  use std::path::Path;
  use std::time::Instant;
+ use std::fs::{self, OpenOptions};
 
  fn main() -> io::Result<()> {
     // Start the timer
@@ -19,10 +20,21 @@
             .open(output_file_path)?;
     }
 
+	let branch_folders: Vec<String> = fs::read_dir(base_dir)?
+		.filter_map(|entry| entry.ok())
+		.filter_map(|entry| {
+			let path = entry.path();
+			if path.is_dir() {
+				Some(path.to_string_lossy().to_string())
+			} else {
+				None
+}})
+		.collect();
+
     let start_time = Instant::now();
 
     // Call the process_input_file function
-    process_input_file(base_dir, output_file_path)?;
+    process_input_file(branch_folders, output_file_path)?;
     let duration = start_time.elapsed();
     println!("Time elapsed: {}\nPhew! I am done.", duration.as_secs_f64());
     Ok(())
